eb2a1eaba774a2c179802fcfb8ee4ba9f4a9a5e4
Benny Chan <bennychan@sillycube.com>
2020-07-13 19:00:52 +0800

    added sitemap.xml

--- js/saasNameApp.js
+++ js/saasNameApp.js
@@ -125,6 +125,8 @@ var vm =  new Vue({
 			var url;
 			if (location.hostname.includes('3sname.com'))
 				url = 'https://api.3sname.com/apis/domains/' + domainsStr + '/';
+			else // dev
+				url = 'https://dev.3sname.com/apis/domains/' + domainsStr + '/';
 			
 			axios
 				.get(url)
@@ -156,6 +158,8 @@ var vm =  new Vue({
 			var vm = this;
 			if (location.hostname.includes('3sname.com'))
 				url = 'https://api.3sname.com/apis/domainPrices/';
+			else // dev
+				url = 'https://dev.3sname.com/apis/domainPrices/';
 				
 			axios
 				.get(url)
